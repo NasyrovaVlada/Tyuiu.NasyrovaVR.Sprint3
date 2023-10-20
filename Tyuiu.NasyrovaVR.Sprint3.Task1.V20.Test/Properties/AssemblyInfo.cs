3f053f0d486ab5583c9958a8eb290577b4f56e48
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-20 19:56:29 +0500

    Task1

---
+++ Tyuiu.NasyrovaVR.Sprint3.Task1.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint3.Task1.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint3.Task1.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7e7e1843-3292-4c78-835c-8bfe959f44e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.NasyrovaVR.Sprint3.Task1.V20/Program.cs
+++ Tyuiu.NasyrovaVR.Sprint3.Task1.V20/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.NasyrovaVR.Sprint3.Task0.V14.Lib;
+using Tyuiu.NasyrovaVR.Sprint3.Task1.V20.Lib;
 
-namespace Tyuiu.NasyrovaVR.Sprint3.Task0.V14
+namespace Tyuiu.NasyrovaVR.Sprint3.Task1.V20
 {
     public class Program
     {
@@ -17,23 +17,22 @@ namespace Tyuiu.NasyrovaVR.Sprint3.Task0.V14
             Console.Title = "Спринт #3 | Выполнила: Насырова В. Р. | ИИПб-23-3";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: Оператор цикла for                                                *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
             Console.WriteLine("* Выполнила: Насырова Влада Ренатовна | ИИПб-23-3                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
-            Console.WriteLine("* ряда по формуле, при n = 2                                              *");
+            Console.WriteLine("* Написать программу используя цикл while, которая вычисляет сумму ряда   *");
+            Console.WriteLine("* по формуле                                                              *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int value = 2;
             int startValue = 1;
-            int stopValue = 5;
-            Console.WriteLine("Переменная X = " + value);
+            int stopValue = 16;
+            
             Console.WriteLine("Старт шага = " + startValue);
             Console.WriteLine("Конец шага = " + stopValue);
 
@@ -41,7 +40,7 @@ namespace Tyuiu.NasyrovaVR.Sprint3.Task0.V14
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(startValue, stopValue));
 
             Console.ReadKey();
         }
