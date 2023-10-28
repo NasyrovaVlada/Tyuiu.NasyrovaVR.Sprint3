0554070ee2d82757be9728bb2c4f57bc05546052
NasyrovaVlada <Tyuiu.NasyrovaVR@outlook.com>
2023-10-28 11:52:53 +0500

    Task7

---
+++ Tyuiu.NasyrovaVR.Sprint3.Task7.V22.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.NasyrovaVR.Sprint3.Task7.V22.Lib
+{
+    public class DataService : ISprint3Task7V22
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            int x;
+            for (x = startValue; x<=stopValue; x++)
+            {
+                y = Math.Round((Math.Sin(x) + (Math.Cos(2 * x) / 2) - 1.5 * x), 2);
+                valueArray[count] = y;
+                count++;
+                if (y == double.NaN || y == double.PositiveInfinity || y == double.NegativeInfinity)
+                {
+                    y = 0;
+                }
+            }
+            
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.NasyrovaVR.Sprint3.Task7.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.NasyrovaVR.Sprint3.Task7.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.NasyrovaVR.Sprint3.Task7.V22.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b0dd1c4-904c-48b0-bc38-f971d3b6b799")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
